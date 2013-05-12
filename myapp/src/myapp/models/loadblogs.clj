a029b1535fb4f2aef5573d4b2b6c57ee6f692298
yikebocai <yikebocai@gmail.com>
2013-05-12 22:46:22 +0800

    can show the markdown blog files

---
+++ myapp/src/myapp/models/loadblogs.clj
@@ -0,0 +1,51 @@
+;;load markdown blog files 
+(ns myapp.models.loadblogs
+   (:use clojure.java.io)
+   (:require [myapp.util :as util] ))
+
+;;define blog object
+(defrecord blog [postdate title content])
+
+(defn read-title [filename]
+	(with-open [rdr (reader filename)]
+		(first (line-seq rdr))))
+
+(defn read-content [filename]
+	(with-open [rdr (reader filename)]
+		(reduce str (doall (line-seq rdr)))))
+
+;;list all blog source files
+(defn list-blog-sources [path]
+  (let [myfile (file path)]
+  	(if-not (.isDirectory myfile ) 
+  	"You should pass a directory path"
+  	;sort by posting date
+  	(sort-by 
+  		#(java.lang.Integer/parseInt (first(clojure.string/split % #"-")))
+  		> 
+  		(.list myfile))))
+)
+
+;;parse date and title ,20130426-darktime_note.md
+;;the first line of the content is title
+(defn load-blog [path]
+	(let [blognames (list-blog-sources path)
+		  cnt (count blognames)]
+      (loop [bloglist []
+      	     i 0]
+      	     (if (< i cnt) 
+      	     	(recur 
+      	     		(conj bloglist 
+							(conj {}
+			  				{:postdate (first(clojure.string/split (nth blognames i) #"-"))}
+			  				{:title (read-title (str path "/" (nth blognames i)))}
+			  				{:content (util/md->html (str "/md/" (nth blognames i)))}
+			  				))
+      	     		(inc i) )
+      	     bloglist))))
+
+		 
+
+
+
+
--- myapp/src/myapp/routes/home.clj
+++ myapp/src/myapp/routes/home.clj
@@ -1,11 +1,13 @@
 (ns myapp.routes.home
   (:use compojure.core)
   (:require [myapp.views.layout :as layout]
-            [myapp.util :as util]))
+            [myapp.util :as util]
+            [myapp.models.loadblogs :as loadblogs]
+            ))
 
 (defn home-page []
   (layout/render
-    "home.html" {:content (util/md->html "/md/docs.md")}))
+    "home.html" {:blogs (loadblogs/load-blog "/Users/zxb/work/blog/src")}))
 
 (defn about-page []
   (layout/render "about.html"))
