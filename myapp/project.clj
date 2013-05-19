4e4fe2905928ad7130e0056ee8b4da743924403f
yikebocai <yikebocai@gmail.com>
2013-05-19 22:45:30 +0800

    add h2 and korma dependencies

--- myapp/project.clj
+++ myapp/project.clj
@@ -8,7 +8,10 @@
                  [clabango "0.5"]
                  [com.taoensso/timbre "1.5.2"]
                  [com.taoensso/tower "1.5.1"]
-                 [markdown-clj "0.9.19"]]
+                 [markdown-clj "0.9.21"]
+                 [com.h2database/h2 "1.3.170"]
+                 [korma "0.3.0-RC5"]
+                 ]
   :plugins [[lein-ring "0.8.3"]]
   :ring {:handler myapp.handler/war-handler
          :init    myapp.handler/init
