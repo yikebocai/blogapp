ad1a3211cb336ff6fd7bb8ff28bf9323c37e8bbc
yikebocai <yikebocai@gmail.com>
2013-06-11 17:34:15 +0800

    add default username and password

--- myapp/src/myapp/models/login.clj
+++ myapp/src/myapp/models/login.clj
@@ -6,17 +6,19 @@
     [noir.session :as session]
     ))
 
+;;if can't find email from db,use default account to valid
 (defn valid [email password]
 	"valid username and password from db,
 	password should be encrypted ahead"
 	(let [myemail (config/get-value "email")
 		mypwd (config/get-value "password")
-		result (and (= myemail email) (= mypwd (util/md5 password)))]
+		result (if (nil? myemail) 
+			(and (= email "admin") (= password "yikebocai")) 
+			(and (= myemail email) (= mypwd (util/md5 password))))]
 		(do 
 			(timbre/info "valid username and password")
 			(cond result (session/put! :username email))
 			(if result true false))))
-
 
 (defn signin [username password]
 	(if (nil? (session/get :username))
