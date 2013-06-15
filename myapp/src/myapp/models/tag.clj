07522e878cd09bb7c521461f5652996692d2a3db
yikebocai <yikebocai@gmail.com>
2013-06-15 20:03:04 +0800

    rm tag.clj

--- myapp/src/myapp/models/tag.clj
+++
@@ -1,8 +0,0 @@
-(ns myapp.models.tag 
-	(:require  
-		[myapp.models.db :as db]
-		[taoensso.timbre :as timbre] ))
-
-(defn list-blog-by-tag [tagname]
-	(let [rs (db/find-blog-by-tag tagname)]
-		()))
